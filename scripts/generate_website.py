d5f452fee2eee0b50f9c8f0564547ee880d3b790
Futrime <futrime@outlook.com>
2023-02-07 18:07:07 +0800

    feat: beautify pages

--- scripts/generate_website.py
+++ scripts/generate_website.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 # Read docs/index.json
 with open('docs/index.json') as f:
@@ -10,21 +11,42 @@ with open('docs/_sidebar.md', 'a') as f_sidebar:
         # Only process 
         f_sidebar.write(f'- [{item}](/{item}.md)\n')
         with open(f'docs/{item}.md', 'w') as f:
-            f.write(f"# {index['index'][item]['name']}\n")
-            f.write(f"\n")
+            repo_user = index['index'][item]['repository'].split('/')[1]
+            repo_name = index['index'][item]['repository'].split('/')[2]
+            tooth_user = index['index'][item]['tooth'].split('/')[1]
+            tooth_name = index['index'][item]['tooth'].split('/')[2]
+
+            f.write(f"# {index['index'][item]['name']}\n\n")
+
+            f.write(f"![GitHub tag (latest SemVer)](https://img.shields.io/github/v/release/{tooth_user}/{tooth_name}?label=VERSION&style=for-the-badge)")
+            f.write("&emsp;")
+            f.write(f"![GitHub Release Date](https://img.shields.io/github/release-date/{tooth_user}/{tooth_name}?label=PUBLISHED&style=for-the-badge)")
+            f.write("&emsp;")
+            if index['index'][item]['license'] != '':
+                license_name = index['index'][item]['license'].replace('-', '--').replace('+', '%2B')
+                f.write(f"![License](https://img.shields.io/badge/license-{license_name}-orange?style=for-the-badge)")
+            else:
+                f.write(f"![Copyright](https://img.shields.io/badge/copyright-{index['index'][item]['author']}-orange?style=for-the-badge)")
+
+
+
+            f.write(f"\n\n")
 
             f.write(f"```shell\n")
             f.write(f"lip install {item}\n")
-            f.write(f"```\n")
-            f.write(f"\n")
+            f.write(f"```\n\n")
 
-            f.write(f"## Metadata\n")
-            f.write(f"- Author: {index['index'][item]['author']}\n")
-            f.write(f"- Homepage: {index['index'][item]['homepage']}\n")
-            f.write(f"- License: {index['index'][item]['license']}\n")
-            f.write(f"- Tooth Path: {index['index'][item]['tooth']}\n")
-            f.write(f"\n")
+            f.write(f"{index['index'][item]['description']}\n\n")
 
-            f.write(f"## Description\n")
-            f.write(f"{index['index'][item]['description']}\n")
-            f.write(f"\n")
+            f.write(f"> Author:&emsp;[{index['index'][item]['author']}](https://github.com/{index['index'][item]['author']})\n")
+            f.write(f">\n")
+            f.write(f"> Homepage:&emsp;[{index['index'][item]['homepage']}]({index['index'][item]['homepage']})\n")
+            f.write(f">\n")
+            f.write(f"> Repository:&emsp;[{index['index'][item]['repository']}](https://{index['index'][item]['repository']})\n\n")
+
+            f.write(f"---\n\n")
+            
+            if os.path.exists(f'readmes/{item}.md'):
+                with open(f'readmes/{item}.md') as f_readme:
+                    readme = f_readme.read()
+                    f.write(f"{readme}\n")
